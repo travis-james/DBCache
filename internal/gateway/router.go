d12ff5dee2aaff141e32736578c21ea7ba00468d
Travis Henderson <tjh@Traviss-MacBook-Air.local>
2025-08-22 14:58:48 +0900

    getData is half finished. I need to add request body for fallback query.

--- internal/gateway/router.go
+++ internal/gateway/router.go
@@ -2,6 +2,7 @@ package gateway
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,23 +14,45 @@ import (
 func NewRouter(grpcClient *grpcInternal.Client) *gin.Engine {
 	router := gin.Default()
 	router.GET("/health", checkHealth(grpcClient))
+	router.GET("/data/:id", getData(grpcClient))
 
 	router.Run("localhost:8080")
 	return router
 }
 
 func checkHealth(grpcClient *grpcInternal.Client) gin.HandlerFunc {
-	return func(c *gin.Context) {
+	return func(gctx *gin.Context) {
 		resp, err := grpcClient.CheckHealth(context.Background(), &pb.Empty{})
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			gctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 		data, err := protojson.Marshal(resp)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			gctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
-		c.Data(http.StatusOK, "application/json", data)
+		gctx.Data(http.StatusOK, "application/json", data)
+	}
+}
+
+func getData(grpcClient *grpcInternal.Client) gin.HandlerFunc {
+	return func(gctx *gin.Context) {
+		resp, err := grpcClient.GetData(context.Background(), &pb.GetRequest{QueryId: gctx.Param("id")})
+		if err != nil {
+			gctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		var respData map[string]any
+		if err := json.Unmarshal([]byte(resp.Data), &respData); err != nil {
+			gctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode response"})
+			return
+		}
+
+		gctx.JSON(http.StatusOK, gin.H{
+			"fromCache": resp.FromCache,
+			"data":      respData,
+		})
 	}
 }
